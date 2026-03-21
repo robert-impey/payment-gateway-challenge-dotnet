6f6e341fa0678252f348c2d7b63110e0efbcbf4a
Robert Impey <robert-impey@users.noreply.github.com>
2026-03-21 21:13:50 +0000

    Stubbed the classes to implement the logic of the program

---
+++ src/PaymentGateway.Api/Services/AcquiringBankClient.cs
@@ -0,0 +1,12 @@
+﻿namespace PaymentGateway.Api.Services
+{
+    // For the code test, the Mountebank simulator is running in Docker when
+    // tests are run.
+    // In a real system, this class might have an interface or inherit from an
+    // abstract class. This would allow mocks to be used in tests.
+    // There might also be different implementations for different banks, or versions of
+    // the bank's API.
+    public class AcquiringBankClient
+    {
+    }
+}
---
+++ src/PaymentGateway.Api/Services/PaymentValidator.cs
@@ -0,0 +1,6 @@
+﻿namespace PaymentGateway.Api.Services
+{
+    public class PaymentValidator
+    {
+    }
+}
